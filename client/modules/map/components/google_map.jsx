c7bd1f06d6da4a8cf3df0b6d6b67edcccd7ff6ac
Idraki Muhamad <idrakimuhamad@gmail.com>
2016-04-11 17:41:18 +0800

    Added custom marker styling

--- client/modules/map/components/google_map.jsx
+++ client/modules/map/components/google_map.jsx
@@ -1,26 +1,6 @@
 import React, { Component} from 'react';
 import shouldPureComponentUpdate from 'react-pure-render/function';
-
 import GoogleMap from 'google-map-react';
-
-const K_WIDTH = 16;
-const K_HEIGHT = 16;
-const styles= {
-  position: 'absolute',
-  width: K_WIDTH,
-  height: K_HEIGHT,
-  left: -K_WIDTH / 2,
-  top: -K_HEIGHT / 2,
-
-  border: '2px solid #f44336',
-  borderRadius: K_HEIGHT,
-  backgroundColor: 'white',
-  textAlign: 'center',
-  color: '#3f51b5',
-  fontSize: 16,
-  fontWeight: 'bold',
-  padding: 4
-};
 
 class GuggleMap extends Component {
     constructor(props) {
@@ -50,7 +30,7 @@ class GuggleMap extends Component {
                     defaultCenter={this.props.center}
                     defaultZoom={this.props.zoom}
                     center={this.state.location}>
-                    <div lat={this.state.location.lat} lng={this.state.location.lng} style={styles} />
+                    <div lat={this.state.location.lat} lng={this.state.location.lng} className="marker" />
                 </GoogleMap>
             </div>
         );
